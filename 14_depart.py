cd0dc96040f8ac8149e09800b4d92c7287018f02
MuhammadRayyan45 <rayyanrk445@gmail.com>
2025-05-26 02:42:30 -0700

    14_depart.py

---
+++ 14_depart.py
@@ -0,0 +1,17 @@
+
+class Employee:
+
+    def __init__(self, name):
+        self.name = name
+
+class Department:
+
+    def __init__(self, employee):
+        self.employee = employee
+
+emp = Employee("rayyan")
+
+dept = Department(emp)
+print(dept.employee.name)
+                
+        
