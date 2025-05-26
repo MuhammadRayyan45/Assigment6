ce5168b9eb8fe72cb6174558a4ef4d2ca5660502
MuhammadRayyan45 <rayyanrk445@gmail.com>
2025-05-26 03:01:36 -0700

    19_callable.py

---
+++ 19_callable.py
@@ -0,0 +1,17 @@
+class Multiplier:
+    def __init__(self, factor):
+        self.factor = factor
+
+    def __call__(self, value):
+        return value * self.factor
+
+
+double = Multiplier(2)
+
+print(callable(double))  
+
+result = double(10)
+print(result)  
+
+triple = Multiplier(3)
+print(triple(5))  
