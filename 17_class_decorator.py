031e91390264bf73c71bfc092c55db9a01492e22
MuhammadRayyan45 <rayyanrk445@gmail.com>
2025-05-26 02:54:22 -0700

    17_class_decorator.py

---
+++ 17_class_decorator.py
@@ -0,0 +1,18 @@
+# Class decorator definition
+def add_greeting(cls):
+  
+    def greet(self):
+        return "Hello from Decorator!"
+    
+    cls.greet = greet  
+    return cls  
+
+@add_greeting
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+
+p = Person("Alice")
+print(p.name)         
+print(p.greet())      
